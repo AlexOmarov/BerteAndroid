574e279498e4c4c970e6419adcb6184b14611870
Alex Omarov <shtil.a@yandex.ru>
2023-02-04 18:46:45 +0700

    AND-1 Create first compose screen

--- app/src/main/kotlin/com/example/berteandroid/MainActivity.kt
+++ app/src/main/kotlin/com/example/berteandroid/MainActivity.kt
@@ -4,22 +4,25 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.berteandroid.ui.theme.BerteAndroidTheme
-import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +45,10 @@ fun FirstScreen() {
         Surface(color = MaterialTheme.colorScheme.background) {
             UserList(
                 listOf(
-                    ElementData("John McFaul", "Foolish description for record"),
+                    ElementData(
+                        "John McFaul",
+                        "Foolish description for record\nFoolish description for record\nFoolish description for record\nFoolish description for record"
+                    ),
                     ElementData("John McFaul", "Foolish description for record"),
                     ElementData("John McFaul", "Foolish description for record"),
                     ElementData("John McFaul", "Foolish description for record"),
@@ -87,7 +93,15 @@ fun UserListRowImage() {
 
 @Composable
 fun UserListRowContent(data: ElementData) {
-    Column {
+    // We keep track if the message is expanded or not in this
+    // variable
+    var isExpanded by remember { mutableStateOf(false) }
+
+    val surfaceColor by animateColorAsState(
+        if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+    )
+
+    Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
         Text(
             text = data.header,
             color = MaterialTheme.colorScheme.secondary,
@@ -96,9 +110,15 @@ fun UserListRowContent(data: ElementData) {
 
         Spacer(modifier = Modifier.height(4.dp))
 
-        Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            shadowElevation = 1.dp,
+            color = surfaceColor,
+            modifier = Modifier.animateContentSize().padding(1.dp)
+        ) {
             Text(
                 text = data.description,
+                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 modifier = Modifier.padding(all = 4.dp),
                 style = MaterialTheme.typography.bodySmall
             )
