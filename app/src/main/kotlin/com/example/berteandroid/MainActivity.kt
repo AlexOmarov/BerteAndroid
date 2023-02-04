b247e34b30751711562b93725f497b23603bd5d2
Alex Omarov <shtil.a@yandex.ru>
2023-02-04 17:11:33 +0700

    AND-1 Create first compose screen

--- app/src/main/kotlin/com/example/berteandroid/MainActivity.kt
+++ app/src/main/kotlin/com/example/berteandroid/MainActivity.kt
@@ -1,42 +1,107 @@
 package com.example.berteandroid
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.berteandroid.ui.theme.BerteAndroidTheme
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BerteAndroidTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContent { FirstScreen() }
+    }
+}
+
+data class ElementData(val header: String, val description: String)
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun FirstScreen() {
+    BerteAndroidTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            UserList(
+                listOf(
+                    ElementData("John McFaul", "Foolish description for record"),
+                    ElementData("John McFaul", "Foolish description for record"),
+                    ElementData("John McFaul", "Foolish description for record"),
+                    ElementData("John McFaul", "Foolish description for record"),
+                )
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun UserList(data: List<ElementData>) {
+    LazyColumn {
+        items(data) { data ->
+            UserListRow(data)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BerteAndroidTheme {
-        Greeting("Android")
+fun UserListRow(data: ElementData) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        UserListRowImage()
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        UserListRowContent(data)
+
+    }
+}
+
+@Composable
+fun UserListRowImage() {
+    Image(
+        painter = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = "Contact profile picture",
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+    )
+}
+
+@Composable
+fun UserListRowContent(data: ElementData) {
+    Column {
+        Text(
+            text = data.header,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Text(
+                text = data.description,
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
     }
 }
